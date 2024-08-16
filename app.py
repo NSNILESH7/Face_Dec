acc30eae89b4dac4494f74b19de2fc0cf822f486
NILESH <nsnilesh@outlook.com>
2024-08-16 23:24:28 +0530

    face

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, Response
 import cv2
-
 app = Flask(__name__)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
